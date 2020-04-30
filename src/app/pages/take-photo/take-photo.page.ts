dee606fd65aec3ff0413b34aa4c4038832c71288
Carlos Eduardo Artioli <caduartioli@gmail.com>
2020-04-30 15:18:22 -0300

    Deploy Firebase

--- src/app/pages/take-photo/take-photo.page.ts
+++ src/app/pages/take-photo/take-photo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-take-photo',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TakePhotoPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    var video = <any>document.getElementById('video');
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: { aspectRatio: 1 } })
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err) {
+          console.log("Não rolou carregar o vídeo");
+        });
+    }
+  }
+
+  takePicture() {
+    var video = <any>document.getElementById('video');
+    var canvas = <any>document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+    context.drawImage(video, 0, 0, 1000, 1000);
+    localStorage.setItem('baltagram.post', JSON.stringify(new Post(canvas.toDataURL(), '', '')));
+
+    video.classList.add("animated");
+    video.classList.add("flash");
+
+    setTimeout(() => {
+      this.navCtrl.navigateForward('/post');
+    }, 1000);
   }
 
 }
